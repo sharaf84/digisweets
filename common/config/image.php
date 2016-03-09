8c72d83cdab872da6ed3d0c95ab1a36cccae51a0
Ahmed Sharaf <a.sharaf@digitreeinc.com>
2016-03-09 15:30:10 +0200

    Add image betsFit, fitWidth & fitHeight

--- common/config/image.php
+++ common/config/image.php
@@ -5,7 +5,16 @@
  * controlles all image sizes and manipulations using yii\imagine\Image class
  * @author Ahmed Sharaf <sharaf.developer@gmail.com>
  */
-//use Yii;
+
+/*ex:
+ * 'sizes' => [
+        'box' => ['thumbnail', 100, 200],
+        'bestFit' => ['bestFit', 100, 200],
+        'fitWidth' => ['fitWidth', 100],
+        'fitHeight' => ['fitHeight', 200],
+    ]
+ */
+
 
 return [
     'placeholders' => [
@@ -13,23 +22,6 @@ return [
         'person' => ['path' =>  Yii::getAlias('@sharedUrl') . '/images/placeholders/', 'filename' => 'person-placeholder.png'],
     ],
     'sizes' => [
-        '25*25' => ['thumbnail', 25, 25],
         'micro' => ['thumbnail', 50, 50],
-        'home-slider' => ['thumbnail', 1231, 673],
-        'home-banner' => ['thumbnail',  1231, 505],
-        'home-product' => ['thumbnail', 207, 395],
-        'home-article' => ['thumbnail', 220, 307],
-        'list-product' => ['thumbnail', 164, 313],
-        'main-product' => ['thumbnail', 251, 496],
-        'bottom-product' => ['thumbnail', 164, 313],
-        'cart-product' => ['thumbnail', 52, 99],
-        'dropdown-product' => ['thumbnail', 69, 131],
-        //'background-product' => ['thumbnail', 251, 496],
-        'list-article' => ['thumbnail', 142, 140],
-        'main-article' => ['thumbnail', 1000, 317],
-        'bottom-article' => ['thumbnail', 303, 197],
-        'dropdown-article' => ['thumbnail', 193, 125],
-        'default_avatar' => ['thumbnail', 100, 100], 
-        'profile_avatar' => ['thumbnail', 228, 228], 
     ]
 ];
